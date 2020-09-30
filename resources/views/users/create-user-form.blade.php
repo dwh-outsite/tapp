25700678de482c43c668beb4d90c29ba0865ffe0
Casper Boone <mail@casperboone.nl>
2020-09-30 21:03:59 +0200

    Add placeholders

--- resources/views/users/create-user-form.blade.php
+++ resources/views/users/create-user-form.blade.php
@@ -10,13 +10,13 @@
     <x-slot name="form">
         <div class="col-span-6 sm:col-span-4">
             <x-jet-label for="name" value="{{ __('Name') }}" />
-            <x-jet-input id="name" type="text" class="mt-1 block w-full" wire:model.defer="name" autofocus />
+            <x-jet-input id="name" type="text" class="mt-1 block w-full" placeholder="{{ __('The name of the bartender') }}" wire:model.defer="name" autofocus />
             <x-jet-input-error for="name" class="mt-2" />
         </div>
 
         <div class="col-span-6 sm:col-span-4">
             <x-jet-label for="email" value="{{ __('Email') }}" />
-            <x-jet-input id="email" type="email" class="mt-1 block w-full" wire:model.defer="email" autofocus />
+            <x-jet-input id="email" type="email" class="mt-1 block w-full" placeholder="{{ __('The email of the bartender') }}" wire:model.defer="email" autofocus />
             <x-jet-input-error for="email" class="mt-2" />
         </div>
     </x-slot>
